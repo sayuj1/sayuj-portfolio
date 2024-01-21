3fa7388bcb32638570c40087450693f4b4ac2906
Sayuj Sehgal <sayuj.sehgal@gmail.com>
2024-01-21 08:24:55 +0000

    initial setup

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white text-6xl font-extrabold mb-4">Hello, I'm Sayuj Sehgal</h1>
+      <p className='text-[#ADB7BE]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic odit, ratione nemo officiis dolore sint.</p>
+    </section>
+  )
+}
+
+export default HeroSection;
